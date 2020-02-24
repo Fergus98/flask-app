7898dbcf2f302431abf5cfdfdf7135626823e67d
Ubuntu <Fergus98@Flask.42m4tj0tozsudhiwu5w434kzea.cwx.internal.cloudapp.net>
2020-02-24 16:04:17 +0000

    added forms

--- application/__init__.py
+++ application/__init__.py
@@ -2,15 +2,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from os import getenv
+
+
  # create a new instance of Flask and store it in app 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
 
 app.config["SQLALCHEMY_DATABASE_URI"] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
  # import the ./application/routes.py fi
 from application import routes
-
-#user - ferg
-#password - password
-#host - 127.0.0.1
-#db name - flaskapp 
+ 
--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,10 @@
  # import render_template function from the flask module
-from flask import render_template
+from flask import render_template, redirect, url_for
  # import the app object from the ./application/__init__.py
-from application import app
+from application import app, db
 
+from application.models import Posts
+from application.forms import PostForm
  # define routes for / & /home, this function will be called when these are accessed
 blogData = [
     {  
@@ -16,11 +18,32 @@ blogData = [
         "content":"This is even more blog data for Flask lectures"
     }
 ]
+@app.route('/posts')
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('posts.html', title='Post', form=form)
 
 @app.route('/')
 @app.route('/home')
 def home():
-     return render_template('home.html', title='Home', posts = blogData)
+    blogData = Posts.query.first()
+    return render_template('home.html', title='Home', posts = blogData)
 
 @app.route('/about')
 def about():
